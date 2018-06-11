64a7f71abd44542f9648c1adfb94af49768b1079
Pavel Panaitov <pavel.panaitov@gmail.com>
2018-06-11 23:24:29 +0300

    Add city temperature on Home page

--- src/components/city/CityList.jsx
+++ src/components/city/CityList.jsx
@@ -4,90 +4,98 @@ import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { Button, Card, Icon } from 'semantic-ui-react';
 import { removeCity } from '../../AC';
+import convertTemperature from '../../utils/convertTemperature';
 import Header from '../city/Header';
 
 const cityBorderColors = [
-	'red',
-	'orange',
-	'green',
-	'blue',
-	'violet',
-	'purple',
-	'pink'
+  'red',
+  'orange',
+  'green',
+  'blue',
+  'violet',
+  'purple',
+  'pink',
 ];
 
 class CityList extends Component {
-	handleToggleAddCityModal = ( id ) => {
-		const { onAddCityModal, onToggleCityModalTitle, onEditCity } = this.props;
-		onToggleCityModalTitle( 'Edit City' );
-		onAddCityModal();
-		onEditCity( id );
-	};
+  handleToggleAddCityModal = (id) => {
+    const { onAddCityModal, onToggleCityModalTitle, onEditCity } = this.props;
+    onToggleCityModalTitle('Edit City');
+    onAddCityModal();
+    onEditCity(id);
+  };
 
-	render() {
-		const max = (cityBorderColors.length - 1);
-		const { entities, removeCity } = this.props;
-		return (
-			(entities.length > 0) &&
-			<Fragment>
-				<Header />
-				<Card.Group>
-					{
-						entities.map( ( item ) => {
-							const color = Math.max( Math.floor( Math.random() * max ) + 1, 0 );
-							return (
-								<Card fluid color={cityBorderColors[color]} key={item.id}>
-									<Card.Header>
-										<h4><NavLink to={`/city/${item.id}`}>{item.name}</NavLink></h4>
-									</Card.Header>
-									<Card.Meta>
-										{item.sys.country}
-									</Card.Meta>
-									<Card.Description>
-										<Button.Group>
-											<Button
-												animated="vertical"
-												positive
-												onClick={() => this.handleToggleAddCityModal( item.id )}
-											>
-												<Button.Content visible>Edit</Button.Content>
-												<Button.Content hidden>
-													<Icon name="edit" />
-												</Button.Content>
-											</Button>
-											<Button.Or />
-											<Button
-												animated="vertical"
-												negative
-												onClick={() => removeCity( item.id )}
-											>
-												<Button.Content visible>Delete</Button.Content>
-												<Button.Content hidden>
-													<Icon name="delete" />
-												</Button.Content>
-											</Button>
-										</Button.Group>
-									</Card.Description>
-								</Card>
-							);
-						} )
-					}
-				</Card.Group>
-			</Fragment>
-		);
-	}
+  render() {
+    const { entities, removeCity } = this.props,
+      ROOT_ICON_URL = 'https://openweathermap.org/img/w',
+      max = (cityBorderColors.length - 1),
+      celsius = convertTemperature('C');
+    
+    return (
+      (entities.length > 0) &&
+      <Fragment>
+        <Header />
+        <Card.Group>
+          {
+            entities.map((item) => {
+              const color = Math.max(Math.floor(Math.random() * max) + 1, 0);
+              return (
+                <Card fluid color={cityBorderColors[color]} key={item.id}>
+                  <Card.Header>
+                    <h4><NavLink to={`/city/${item.id}`}>{item.name}</NavLink></h4>
+                  </Card.Header>
+                  <Card.Meta>
+                    {item.sys.country}
+                  </Card.Meta>
+                  <Card.Description>
+                    <img src={`${ROOT_ICON_URL}/${item.weather[0].icon}.png`} alt={`Weather ${item.name}, ${item.sys.country}`} />
+                    <span>{celsius(item.main.temp)} °C</span>
+                  </Card.Description>
+                  <Card.Description>
+                    <Button.Group>
+                      <Button
+                        animated="vertical"
+                        positive
+                        onClick={() => this.handleToggleAddCityModal(item.id)}
+                      >
+                        <Button.Content visible>Edit</Button.Content>
+                        <Button.Content hidden>
+                          <Icon name="edit" />
+                        </Button.Content>
+                      </Button>
+                      <Button.Or />
+                      <Button
+                        animated="vertical"
+                        negative
+                        onClick={() => removeCity(item.id)}
+                      >
+                        <Button.Content visible>Delete</Button.Content>
+                        <Button.Content hidden>
+                          <Icon name="delete" />
+                        </Button.Content>
+                      </Button>
+                    </Button.Group>
+                  </Card.Description>
+                </Card>
+              );
+            })
+          }
+        </Card.Group>
+      </Fragment>
+    );
+  }
 };
 
 CityList.propTypes = {
-	entities              : PropTypes.array.isRequired,
-	onAddCityModal        : PropTypes.func.isRequired,
-	onToggleCityModalTitle: PropTypes.func.isRequired,
-	removeCity            : PropTypes.func.isRequired
-}
+  entities: PropTypes.array.isRequired,
+  onAddCityModal: PropTypes.func.isRequired,
+  onToggleCityModalTitle: PropTypes.func.isRequired,
+  removeCity: PropTypes.func.isRequired,
+};
 
 export default connect(
-	( { weather } ) => ({
-		entities: weather.entities
-	}),
-	{ removeCity }
-)( CityList );
+  ({ weather }) => ({
+    entities: weather.entities,
+  }),
+  { removeCity },
+)(CityList);
--- src/components/city/Header.jsx
+++ src/components/city/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
       <div className="city-header__inner">
         <h2 className="city-header__title">City</h2>
         <h2 className="city-header__title">Country</h2>
+        <h2 className="city-header__title">Temperature</h2>
       </div>
     </div>
   );
